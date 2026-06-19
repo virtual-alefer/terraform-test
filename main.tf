daf595640e6b758d2db218a5c0f5d67fde20c59e
unknown <virtual.alefer@email.com>
2026-06-19 00:13:39 -0600

    Added security group and attached to instance

--- main.tf
+++ main.tf
@@ -5,19 +5,24 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
-
+    vpc_security_group_ids = [aws_security_group.instance.id]
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World!" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+    user_data_replace_on_change = true
     tags = {
         Name = "ExampleInstance"
     }
 }
 
+resource "aws_security_group" "instance" {
+    name        = "terraform-example-instance"
+    description = "Allow HTTP traffic on port 8080"
 
-
-/* 
-
-resource "<PROVIDER>_<TYPE>"  "<NAME>" {
-[CONFIGURATION]
-}
-
-
-*/
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
