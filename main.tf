6401d0b36ac17d3ed93a812b37cdf8b1c52b2a29
unknown <virtual.alefer@email.com>
2026-06-18 22:18:05 -0600

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-0c55b159cbfafe1f0"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "ExampleInstance"
+    }
+}
+
+
+
+/* 
+
+resource "<PROVIDER>_<TYPE>"  "<NAME>" {
+[CONFIGURATION]
+}
+
+
+*/
